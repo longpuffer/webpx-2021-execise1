91dbff16f529995ed26e517e58d20a242c7cb0e1
lopngpuffer <70842199+lopngpuffer@users.noreply.github.com>
2021-09-27 16:45:13 +0900

    fisrt my website

---
+++ scripts/main.js
@@ -0,0 +1,36 @@
+const myHeading = document.querySelector('h1');
+myHeading.textContent = 'Mozilla is cool.';
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/sans.png') {
+      myImage.setAttribute ('src','images/unchi.png');
+    } else {
+      myImage.setAttribute ('src','images/sans.png');
+    }
+}
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName(){
+  let myName= prompt('Please enter your name');
+  if(!myName){
+    setUserName();
+  }else{
+    localStorage.setItem('name',myName);
+    myHeading.innerHTML = 'Mozilla is cool, '+myName;
+  }
+}
+
+if(!localStorage.getItem('name')){
+  setUserName();
+}else{
+  let storedName=localStorage.getItem('name');
+  myHeading.innerHTML='Mozilla is cool,' + storedName;
+}
+
+myButton.onclick=function(){
+  setUserName();
+};
